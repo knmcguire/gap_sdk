489fb01c196cd4b03ee13b7138c3a11aaf9491bf
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-10-21 11:12:19 +0200

    Updated bsp for ai deck

---
+++ configs/ai_deck.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+export GAP_SDK_HOME="$(dirname $(dirname "$(readlink -f "${BASH_SOURCE[0]}")"))"
+
+export TARGET_CHIP_FAMILY="GAP8"
+export TARGET_CHIP="GAP8"
+export TARGET_NAME="gap"
+export BOARD_NAME=ai_deck
+export PULP_CURRENT_CONFIG=$BOARD_NAME@config_file=config/$BOARD_NAME.json
+export GVSOC_CONFIG=$BOARD_NAME
+
+source $GAP_SDK_HOME/configs/common.sh
